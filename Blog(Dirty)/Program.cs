ef605a14cc37d47c0be0ee28fe6d7e00d1921c02
mateuszGrzelak-git <142941539+mateuszGrzelak-git@users.noreply.github.com>
2024-07-10 14:58:30 +0200

    Added database

--- Blog(Dirty)/Program.cs
+++ Blog(Dirty)/Program.cs
@@ -1,8 +1,17 @@
 ﻿using Blog_Dirty_;
 
-Console.Write("Write a username: ");
-string username = Console.ReadLine();
-Console.Write("Write a password: ");
-string password = Console.ReadLine();
+namespace Blog_Dirty_
+{
+    sealed class Program
+    {
+        private static UserManager userManager = new UserManager();
+        static void Main(string[] args)
+        {
+            Console.Write("Write a username: ");
+            string username = Console.ReadLine();
+            Console.Write("Write a password: ");
+            string password = Console.ReadLine();
+        }
+    }
 
-
+}
