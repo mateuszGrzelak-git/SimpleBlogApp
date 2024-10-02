cce727a8803d185bea4057395b62d95fff038dad
mateuszGrzelak-git <142941539+mateuszGrzelak-git@users.noreply.github.com>
2024-10-02 09:24:45 +0200

    Added multi 'text inputs'

--- Blog/Pages/FrontEnd/blogPage/blog.cshtml.cs
+++ Blog/Pages/FrontEnd/blogPage/blog.cshtml.cs
@@ -1,3 +1,5 @@
+using Azure.Core;
+using Blog.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,6 +9,13 @@ namespace Blog.FrontEnd.blogPage
     {
         public void OnGet()
         {
+            
+        }
+        protected void Submit(object sender, EventArgs e)
+        {
+            string blogData = Request.Form["blogContainment"];
+            DatabaseManagement databaseManagement = new DatabaseManagement("");
+            databaseManagement.addValue();
         }
     }
 }
---
+++ Blog/wwwroot/js/BlogPostHandler.js
@@ -0,0 +1,7 @@
+﻿var cmd = '';
+function clickHandle() {
+    cmd += '    <input type=\"text\" id=\"blogContainment\" />';
+    document.getElementById("moreBlogs").innerHTML = cmd;
+}
+
+var button = document.getElementById("submitButton").addEventListener("click", clickHandle);
