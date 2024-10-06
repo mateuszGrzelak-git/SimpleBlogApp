1627e31544792a858079807a9296b024315ccace
mateuszGrzelak-git <142941539+mateuszGrzelak-git@users.noreply.github.com>
2024-10-06 12:53:36 +0200

    Added statements in order to blog

--- Blog/Pages/FrontEnd/blogPage/blog.cshtml.cs
+++ Blog/Pages/FrontEnd/blogPage/blog.cshtml.cs
@@ -1,11 +1,14 @@
 using Blog.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog.FrontEnd.blogPage
 {
     public class blogModel : PageModel
     {
+        [BindProperty]
+        public string BlogTitle { get; set; }
         [BindProperty]
         public string BlogData { get; set; }
 
@@ -24,15 +27,31 @@ namespace Blog.FrontEnd.blogPage
             // Logika dla przesy³ania formularza
             DatabaseManagement databaseManagement = new DatabaseManagement("Data Source=(local)\\POSTSDATABASE;Initial Catalog=PostsRepository;Integrated Security=True;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;encrypt=false");
 
-            if(databaseManagement.getValue(1)==null)
+            int lastId = databaseManagement.GetLastId();
+
+            if (lastId == 0)
             {
-                RedirectToPage("/FrontEnd/homePage/home");
+                Console.WriteLine("Id Equal 0");
+                // Obs³u¿ brak wpisów w tabeli Users (np. przekierowanie lub komunikat o b³êdzie)
+                return RedirectToPage("/FrontEnd/homePage/home");
             }
 
-            databaseManagement.addValue("Admin", "Default", BlogData);
+            string author = databaseManagement.getValue(lastId);
 
-            return RedirectToPage("/FrontEnd/homePage/home"); // lub inna akcja po zapisie danych
+            if (string.IsNullOrEmpty(author))
+            {
+                Console.WriteLine("Author empty");
+                return RedirectToPage("/FrontEnd/homePage/home");
+            }
+            else
+            {
+                Console.WriteLine("Success");
+                databaseManagement.addValue(author, BlogTitle, BlogData);
+
+                return RedirectToPage("/FrontEnd/homePage/home"); // lub inna akcja po zapisie danych
+            }
         }
+
 
     }
 }
